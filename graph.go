e1c4fa57737c92e5c19c40e360f9ed561bf3837d
Aryan <aryanaman97@gmail.com>
2026-02-03 00:04:18 +0530

    changes for graph

--- graph.go
+++ graph.go
@@ -19,6 +19,32 @@ func dfs(node int , graph map[int][]int , visited map[int]bool){
 }
 
 
+func bfs( start int , graph map[int][]int){
+	visited := make(map[int]bool)
+
+	queue:=[]int{start}
+
+	visited[start]=true
+
+	for len(queue)>0{
+		node:=queue[0]
+		queue=queue[1:]
+
+		fmt.Println(node , "bfs")
+
+		for _, neighbour := range graph[node]{
+
+			if !visited[neighbour]{
+				visited[neighbour]=true
+
+				queue=append(queue,neighbour)
+			}
+		}
+
+
+	}
+}
+
 
 
 func main(){
@@ -37,6 +63,9 @@ func main(){
 		fmt.Println("DFS traversal starting from node 2:")
 	dfs(2,graph,visited)
 		fmt.Println()
+
+
+    bfs(0,graph)
 
 
 

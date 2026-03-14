a6040f5b0a5b909de7f58fe3653632391d8c2f87
Aryan <aryanaman97@gmail.com>
2026-03-15 01:19:59 +0530

    changes for strings

---
+++ longestSubs.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func subStr( s string){
+
+	hashMap := map[byte]bool{}
+
+	left:=0
+
+	minLen:=0
+	for right:=0;right<len(s);right++{
+
+
+		for hashMap[s[right]]{
+
+			delete(hashMap,s[left])
+			left++
+		}
+
+		hashMap[s[right]]=true
+
+		if right-left+1 > minLen{
+			minLen=right-left+1
+		}
+	}
+
+fmt.Println(minLen)
+
+}
+
+func main(){
+	s := "abcabcbb"
+
+	subStr(s)
+}

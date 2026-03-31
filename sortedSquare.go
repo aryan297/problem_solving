ec7f8c28732ed5f09744a2c5f4a227a4696dd3ca
Aryan <aryanaman97@gmail.com>
2026-04-01 00:53:29 +0530

    changes for sorted square

---
+++ sortedSquare.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func sorted(arr []int){
+left :=0
+right :=len(arr)-1
+
+pos:=len(arr)-1
+res :=make([]int,len(arr))
+
+for left <right{
+
+	if arr[left] * arr[left] > arr[right]*arr[right] {
+		res[pos]=arr[left] *arr[left]
+		left++
+
+	} else{
+
+		res[pos]=arr[right]*arr[right]
+		right--
+	}
+
+	pos--
+
+
+}
+
+fmt.Println(res)
+
+}
+
+func main(){
+	arr :=[]int{4,-1,0,3,10}
+	sorted(arr)
+}
